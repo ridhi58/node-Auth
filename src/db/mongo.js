b9b3319f7ed110926b1a23a85ad9f3b1cf67debf
ridhi58 <ridhiagarwal58@gmail.com>
2021-01-07 19:25:40 +0530

    updated

--- src/db/mongo.js
+++ src/db/mongo.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/task-2',
+mongoose.connect('mongodb://127.0.0.1:27017/task-2',
 {
-    useNewUrlParser:true, useCreateIndex:true,useUnifiedTopology:false
+    useNewUrlParser:true, useCreateIndex:true,useUnifiedTopology:true
 })
