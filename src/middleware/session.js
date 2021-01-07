9e32accf968b2bc4d3683d5295a534561f8184be
ridhi58 <ridhiagarwal58@gmail.com>
2021-01-07 20:07:35 +0530

    updated

--- src/middleware/session.js
+++ src/middleware/session.js
@@ -6,19 +6,19 @@ const session = require('express-session')
 
 module.exports = session({
     name:"mySession",
-    key:"session-key",
+    key:process.env.SESSION_KEY,
     // store:new redisStore({
     //     host:process.env.REDIS_IP,
     //     port:process.env.REDIS_PORT,
     //     client:redisclient
     // }),
-    secret:'session-secret',
+    secret:process.env.SESSION_SECRET,
     resave:false,
     saveUninitialized:false,
     cookie:{
         maxAge:10000*60,
         sameSite:true,
-        secure:true
+        secure:false
         // domain:'localhost'
     }
 })
